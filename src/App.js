4942d4bdfd3bfaf2bd293339f526d1fbe8589d31
alejandro <alitux.da@gmail.com>
2022-07-26 23:44:25 -0300

    entregable react 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar';
+import './index.css';
+
+const App = ()=> {
+    return (
+    <>
+        <NavBar/>
+        <footer>pie de pagina</footer>
+    </>
+    )
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = ()=> {
+
+
+    return (
+    <>
+  <header>
+		
+	</header>
+	<nav>
+        <ul>
+            <li className="menuLeft"><a href="#logo">JuegueteriaGalarza</a></li>
+            <li className="menuRight"><a href="#menuContact">contact</a></li>
+            <li className="menuRight"><a href="#menuCatalogo">catalogo</a></li>
+            <li className="menuRight"><a href="#menuBuscador">buscador</a></li>
+        </ul>
+     </nav> 
+    </>
+    )
+}
+
+export default NavBar;
